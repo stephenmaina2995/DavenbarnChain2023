828eb54eb72bc18622e0614d4e791b5a9cb3cd5b
stephenmaina2995 <stephenmaina2995@gmail.com>
2023-11-11 08:06:26 +0300

    Added structs

---
+++ backend/contracts/Twitter-Struct.sol
@@ -0,0 +1,35 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.7.4;
+
+pragma experimental ABIEncoderV2;
+
+contract Twitter {
+
+    struct Tweet {
+       address author;
+        string content;
+        uint256 timestamp;
+        uint256 likes;
+    }
+
+    mapping(address => Tweet[]) public tweets;
+
+    function createTweet(string memory _tweet) public {
+
+        Tweet memory newTweet = Tweet({
+            author: msg.sender,
+            content: _tweet,
+            timestamp: block.timestamp,
+            likes: 0
+        });
+
+        tweets[msg.sender].push(newTweet);
+    }
+    function getTweet(address _owner, uint _i) public view returns (Tweet memory){
+        return tweets[_owner][_i];
+    }
+    function getAllTweets(address _owner) public view returns (Tweet[] memory){
+        return tweets[_owner];
+    }
+}
